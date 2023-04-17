149a236ff8dab3a3e3131233284ae37b836b3ec7
luizanurnberg <nurnbergluiza@gmail.com>
2023-04-17 12:36:35 -0300

    quarta parte: ajustes de linha finais antes da entrega

--- exceptions/EAPI/apiExceptions.ts
+++ exceptions/EAPI/apiExceptions.ts
@@ -2,9 +2,8 @@ class EAPI {
     static invalidRouteException() {
         return { msg: 'Você está tentando acessar uma rota inválida!' };
     }
-
     static errorFromSystemException() {
-        return { msg: 'Algo de errado aconteceu, tente novamente ou entre em contato com o fornecedor da aplicação' };
+        return { msg: 'Algo de errado aconteceu, tente novamente ou entre em contato com o fornecedor da aplicação!' };
     }
 }
 
